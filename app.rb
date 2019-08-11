c73498ded32ed692771c139a460e48e34c029454
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-11 23:33:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do #sets up a homepage '/'
+  "Hello, World!"
+  end
+  
+  get '/name' do #sets up a homepage '/'
+  "My name is Manzo"
+  end
+  
+  get '/hometown' do #sets up a homepage '/'
+  "My hometown is Empire"
+  end
+  
+  get '/favorite-song' do #sets up a homepage '/'
+  "My favorite song is Today was good day"
+  end
+  
 end
