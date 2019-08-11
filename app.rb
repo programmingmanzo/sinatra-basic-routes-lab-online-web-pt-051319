9896681c7d1c4e5eb78086c9431a370dbcd07d35
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-11 23:33:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,15 +6,15 @@ class App < Sinatra::Base
   "Hello, World!"
   end
   
-  get '/name' do #sets up a homepage '/'
+  get '/name' do 
   "My name is Manzo"
   end
   
-  get '/hometown' do #sets up a homepage '/'
+  get '/hometown' do 
   "My hometown is Empire"
   end
   
-  get '/favorite-song' do #sets up a homepage '/'
+  get '/favorite-song' do 
   "My favorite song is Today was good day"
   end
   
